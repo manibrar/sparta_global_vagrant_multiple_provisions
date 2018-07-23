c1543ec806e29142db0590ac03b6d4aee8bb8fbe
manibrar <manvirbrar1988@gmail.com>
2018-07-23 23:35:29 +0100

    mongo update

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -5,4 +5,9 @@ sudo apt-get install build-essential libgmp-dev libxml2 -y
 sudo apt-get install nginx -y
 
 # install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt-get update
 sudo apt-get install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
