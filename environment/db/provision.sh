f19ddc28d30c2654958d7c379fefa0ecfc916c95
manibrar <manvirbrar1988@gmail.com>
2018-07-23 23:15:57 +0100

    readme added

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -1,6 +1,8 @@
 
 sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install build-essential libgmp-dev libxml2 -y
+sudo apt-get install nginx -y
 
 # install mongodb
 sudo apt-get install -y mongodb-org
