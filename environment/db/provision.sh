fcf2e113dcb0be13c92d68f937c48ef637c3b4d5
manibrar <manvirbrar1988@gmail.com>
2018-07-24 00:08:11 +0100

    mongo db and rake install

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -9,5 +9,3 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF50
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
